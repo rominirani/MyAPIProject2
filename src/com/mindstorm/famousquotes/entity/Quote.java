27766ccf0b37a7c6e7e57ad7cb4b185949184de9
Romin Irani <romin.irani@mindstormsoftware.com>
2014-01-12 10:43:36 +0530

    Initial Version

---
+++ src/com/mindstorm/famousquotes/entity/Quote.java
@@ -0,0 +1,43 @@
+package com.mindstorm.famousquotes.entity;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class Quote {
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	Long id;
+	@Persistent
+	String author;
+	@Persistent
+	String message;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
